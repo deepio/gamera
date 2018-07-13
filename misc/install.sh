22740af98d1acf232687be0d914c8f5890ec6e36
deepio <alex.daigle2@gmail.com>
2018-07-13 09:53:54 -0400

    Fixed: Eval typo for shell script.

--- misc/install.sh
+++ misc/install.sh
@@ -3,7 +3,7 @@ set -o errexit # Exit immediately if a command exits with a non-zero status.
 set -o nounset # Treat unset variables as an error when substituting.
 set -o xtrace # Print commands and their arguments as they are executed.
 
-eval "${MATRIX_EVAL}"
+eval $MATRIX_EVAL
 pip install -U pip
 pip install pytest docutils pygments
 
